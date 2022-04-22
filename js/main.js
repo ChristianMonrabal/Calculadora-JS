c7e4e9f433be031fc79136a86516992523ab2ba0
Christian Monrabal <100007473.joan23@fje.edu>
2022-04-22 16:29:45 +0200

    configures

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 function calcular() {
     var Valor1 = document.getElementById('valor1').value
     var Valor2 = document.getElementById('valor2').value
-
-
     var operacion = document.getElementById('operacion').value
-    alert('Voy a: ' + operacion + 'los valores ' + Valor1 + ' y ' + Valor2);
+
+    if (operacion == "sumar") {
+        var resultado = Number(Valor1) + Number(Valor2)
+    } else if (operacion == "restar") {
+        var resultado = Number(Valor1) - Number(Valor2)
+    } else if (operacion == "multiplicar") {
+        var resultado = Number(Valor1) * Number(Valor2)
+    } else {
+        var resultado = Number(Valor1) / Number(Valor2)
+    }
+
+    var parrafo = document.getElementById('resultado')
+    parrafo.innerHTML = "El resultado es:" + resultado
+
 }
