affe13000ac191f2223041e26e6abd77c7c86460
Christian Monrabal <100007473.joan23@fje.edu>
2022-04-22 15:49:40 +0200

    first configuration

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function calcular() {
+    var Valor1 = document.getElementById('valor1').value
+    var Valor2 = document.getElementById('valor2').value
+
+
+    var operacion = document.getElementById('operacion').value
+    alert('Voy a: ' + operacion + 'los valores ' + Valor1 + ' y ' + Valor2);
+}
